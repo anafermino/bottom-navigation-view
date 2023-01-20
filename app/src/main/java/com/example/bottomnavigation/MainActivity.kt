9ed12bb297858f5e67c981302b157edd53a7aeff
Ana Lucia Fermino de O. Arine <CIANDT\anafermino@lnb018676cps.ciandt.global>
2023-01-20 17:11:06 -0300

    Bottom navigation with fragments created

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        val thirdFragment = ThirdFragment()
+
+        setCurrentFragment(firstFragment) //set the initial fragment that shows app at the app's opening
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.miHome -> setCurrentFragment(firstFragment)
+                R.id.miMessages -> setCurrentFragment(secondFragment)
+                R.id.miProfile -> setCurrentFragment(thirdFragment)
+            }
+            true
+        }
+
+        // Para mostrar balãozinho com número de mensagens recebidas, por exemplo
+        bottomNavView.getOrCreateBadge(R.id.miMessages).apply {
+            number = 10     // número pro balão
+            isVisible = true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
+}
